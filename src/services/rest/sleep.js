6d34c2cf408157065a8515613e5b65535d5379fc
Omar Matthew <omar.matthew@gmail.com>
2025-04-28 14:46:53 +0200

    Added List sort, moved formateDate function to /src/services/utils.js, and functions returns List Object for the SleepDataScreen and SleepDataSummaryScreen.

--- src/services/rest/sleep.js
+++ src/services/rest/sleep.js
@@ -1,4 +1,5 @@
-import {END_DATE, LAST_48_HOURS, START_DATE, TOKEN_SLEEP_URL} from './config';
+import {END_DATE, START_DATE, TOKEN_SLEEP_URL} from './config';
+import {convertSecondsToHMSS, formatDateYYYYMMDD} from '../utils';
 
 // See: https://developer.withings.com/api-reference/#tag/sleep/operation/sleepv2-get
 export const fetchSleepData = async (accessToken) => {
@@ -26,30 +27,34 @@ export const fetchSleepData = async (accessToken) => {
         console.log('Sleep Data Response:', data);
         if (data.status === 0 && data.body.series && data.body.series.length > 0) {
             console.log(`✅ Retrieved ${data.body.series.length} sleep data points:`);
-                data.body.series.forEach((entry, index) => {
-                    const start = new Date(entry.startdate * 1000); // Convert from seconds
-                    const end = new Date(entry.enddate * 1000);
-                    const state = entry.state; // Sleep stage
-                    const hr = entry.hr;
-                    const rr = entry.rr;
-                    const wi = entry.withings_index;
+            // Sort the entries by the latest date (descending order)
+            data.body.series.sort((a, b) => b.startdate - a.startdate);
+            data.body.series.forEach((entry, index) => {
+                const start = new Date(entry.startdate * 1000); // Convert from seconds
+                const end = new Date(entry.enddate * 1000);
+                const state = entry.state; // Sleep stage
+                const hr = entry.hr;
+                const rr = entry.rr;
+                const wi = entry.withings_index;
 
-                    console.log(`\n--- 📊 Sleep Point ${index + 1} ---`);
-                    console.log('🕒 Start:', start.toISOString());
-                    console.log('🕓 End:', end.toISOString());
-                    console.log('💤 State:', getSleepStateName(state));
-                    if (hr !== undefined) {
-                        console.log('❤️ HR:', hr);
-                        Object.entries(hr).forEach(([timestamp, value]) => {
-                            const time = new Date(Number(timestamp) * 1000).toISOString();
-                            console.log(`   🕒 ${time} → ❤️ ${value} bpm`);
-                        });
-                    }
-                    if (rr !== undefined) {console.log('🌬️ RR:', rr);}
-                    if (wi !== undefined) {console.log('📈 Withings Index:', wi);}
-                });
+                console.log(`\n--- 📊 Sleep Point ${index + 1} ---`);
+                console.log('🕒 Start:', start.toISOString());
+                console.log('🕓 End:', end.toISOString());
+                console.log('💤 State:', getSleepStateName(state));
+                if (hr !== undefined) {
+                    console.log('❤️ HR:', hr);
+                    Object.entries(hr).forEach(([timestamp, value]) => {
+                        const time = new Date(Number(timestamp) * 1000).toISOString();
+                        console.log(`   🕒 ${time} → ❤️ ${value} bpm`);
+                    });
+                }
+                if (rr !== undefined) {console.log('🌬️ RR:', rr);}
+                if (wi !== undefined) {console.log('📈 Withings Index:', wi);}
+            });
+            return data;
         } else {
             console.warn('⚠️ No sleep data available.');
+            return null;
         }
 
     } catch (error) {
@@ -71,11 +76,11 @@ export const fetchSleepDataSummary = async (accessToken) => {
             },
             body: new URLSearchParams({
                 action: action,
-                startdateymd: formatDate(START_DATE),
-                enddateymd: formatDate(END_DATE),
+                startdateymd: formatDateYYYYMMDD(START_DATE),
+                enddateymd: formatDateYYYYMMDD(END_DATE),
                 // Store timestamp for last update of data to fetch entries after this date, ideally startDate + endDate
                 // Set as startDate for testing
-                lastupdate: LAST_48_HOURS,
+                lastupdate: START_DATE,
                 access_token: accessToken,
             }).toString(),
         });
@@ -87,24 +92,30 @@ export const fetchSleepDataSummary = async (accessToken) => {
         if (data.status === 0 && data.body.series && data.body.series.length > 0) {
             console.log(`✅ Retrieved ${data.body.series.length} sleep entries:`);
 
+            // Sort the entries by the latest date (descending order)
+            data.body.series.sort((a, b) => b.startdate - a.startdate);
+
             data.body.series.forEach((entry, index) => {
                 console.log(`\n--- 💤 Sleep Entry ${index + 1} ---`);
                 console.log('Date:', new Date(entry.date).toDateString());
-                console.log('  - Total Time in Bed:', entry.data.total_timeinbed, 'seconds');
-                console.log('  - Total Sleep Time:', entry.data.total_sleep_time, 'seconds');
-                console.log('  - Deep Sleep Duration:', entry.data.deepsleepduration + 'seconds');
-                console.log('  - Light Sleep Duration:', entry.data.lightsleepduration + 'seconds');
-                console.log('  - Sleep Efficiency:', (entry.data.sleep_efficiency * 100 )+ '%');
-                console.log('  - Duration to Sleep:', entry.data.durationtosleep, 'seconds');
-                console.log('  - Duration to Wake:', entry.data.wakeupduration, 'seconds');
+                console.log('  - Total Time in Bed:', convertSecondsToHMSS(entry.data.total_timeinbed));
+                console.log('  - Total Sleep Time:', convertSecondsToHMSS(entry.data.total_sleep_time));
+                console.log('  - Deep Sleep Duration:', convertSecondsToHMSS(entry.data.deepsleepduration));
+                console.log('  - Light Sleep Duration:', convertSecondsToHMSS(entry.data.lightsleepduration));
+                console.log('  - Sleep Efficiency:', (entry.data.sleep_efficiency * 100 ) + '%');
+                console.log('  - Duration to Sleep:', convertSecondsToHMSS(entry.data.durationtosleep));
+                console.log('  - Duration to Wake:', convertSecondsToHMSS(entry.data.wakeupduration));
                 console.log('  - Average Heart Rate:', entry.hr_average);
                 console.log('  - Min Heart Rate:', entry.data.hr_min);
                 console.log('  - Max Heart Rate:', entry.data.hr_max);
                 console.log('  - Sleep Start:', new Date(entry.startdate * 1000).toISOString());
                 console.log('  - Sleep End:', new Date(entry.enddate * 1000).toISOString());
             });
+
+            return data;
         } else {
             console.warn('⚠️ No sleep summary data available.');
+            return null;
         }
     } catch (error) {
         console.error('❌ Error fetching Sleep Data Summary:', error);
@@ -112,13 +123,8 @@ export const fetchSleepDataSummary = async (accessToken) => {
     }
 };
 
-const formatDate = (unixTimestamp) => {
-    const date = new Date(unixTimestamp * 1000); // Convert from seconds to ms
-    return date.toISOString().split('T')[0]; // Get 'yyyy-mm-dd'
-};
-
 // Optional helper to give human-readable state names
-const getSleepStateName = (state) => {
+export const getSleepStateName = (state) => {
     const states = {
         0: 'Unknown',
         1: 'Awake',
